d34dada563d9757b06ebf64b84e086e3959d5300
DIVYANSHU KUMAR <debu.chatra@gmail.com>
2024-06-27 20:24:04 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+const quotes = [
+  '"Mathematics is the queen of science." - Carl Friedrich Gauss',
+  '"Without mathematics, there is nothing you can do. Everything around you is mathematics." - Shakuntala Devi',
+  '"God is subtle but not malicious." - Albert Einstein (referring to the beauty and elegance of mathematics)',
